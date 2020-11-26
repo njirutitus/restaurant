3323ddc60bd14068a24698f9747198ce67db2b12
titus njiru <njirutitus@gmail.com>
2020-11-26 15:40:48 +0300

    Font-awesome offline

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ const content = document.getElementById("main-content");
 function sidebarToggler() {
   if (sidebar.style.display == "none") {
     sidebar.style.display = "block";
-    sidebar.style.width = "100%";
+    content.style.width = "80%";
     sidebar.style.zIndex = "99999";
   } else {
     sidebar.style.display = "none";
